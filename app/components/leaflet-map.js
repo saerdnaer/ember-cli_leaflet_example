a42f93623c9c6ccccb67431ef31159f0319c4c2b
Andreas Hubel <andi@saerdnaer.de>
2015-05-18 15:52:12 +0200

    added custom default position and zoom level

--- app/components/leaflet-map.js
+++ app/components/leaflet-map.js
@@ -3,6 +3,9 @@ import EmberLeafletComponent from 'ember-leaflet/components/leaflet-map';
 import TileLayer from 'ember-leaflet/layers/tile';
 
 export default EmberLeafletComponent.extend({
+  center: L.latLng(48.2628, 11.6685),
+  zoom: 18, 
+  options: {maxZoom: 19, minZoom: 0},
   childLayers: [
     TileLayer.extend({
       tileUrl: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
